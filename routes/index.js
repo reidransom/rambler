795da22b68eb38ea48b41c2a7ee5a5eee7e201d2
Reid Ransom <reid@reidransom.com>
2014-03-11 21:15:23 -0700

    more useful limit on number of notes displayed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var db = require('../models')
 exports.index = function (req, res) {
     db.Note.findAndCountAll({
         offset: 0,
-        limit: 5
+        limit: 50
     })
     .success(function (notes) {
     	res.render('index', {
