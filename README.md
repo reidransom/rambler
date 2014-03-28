071be87e23b7153761290bff78d7045c579b8db7
Reid Ransom <reid@reidransom.com>
2014-03-27 20:57:32 -0700

    housekeeping

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 # Ramblings
 
-## Roadmap
-
-* ~~SQLite database~~
-* Better design
-* Look ahead typing - [typeahead.js](http://twitter.github.io/typeahead.js/)
-* Calendar-based scroll
-* Thumbnail scrollbar
-
 ## Dev
 
     $ npm install supervisor -g
     $ supervisor -w server.js server.js
 
+## Test
+
+    $ npm test
--- models/index.js
+++ models/index.js
@@ -1,14 +1,16 @@
+/* jshint node: true */
+
 var fs        = require('fs'),
     path      = require('path'),
     Sequelize = require('sequelize'),
     lodash    = require('lodash'),
-    app       = require('../app'),
     sequelize = new Sequelize(null, null, null, {
         dialect: 'sqlite',
         storage: process.env.STORAGE || path.join(__dirname, '..', 'data.db')
     }),
     db        = {}
- 
+
+// Initialize all the model/*.js files 
 fs
     .readdirSync(__dirname)
     .filter(function(file) {
@@ -18,7 +20,6 @@ fs
         var model = sequelize.import(path.join(__dirname, file))
         db[model.name] = model
     })
- 
 Object.keys(db).forEach(function(modelName) {
     if (db[modelName].options.hasOwnProperty('associate')) {
         db[modelName].options.associate(db)
