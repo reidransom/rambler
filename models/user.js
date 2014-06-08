c3f193f0fdd5598d21b5866a0272e8e861f8615e
Reid Ransom <reid@reidransom.com>
2014-06-07 18:44:15 -0700

    restructured the user class functions
    included them in the optional argument to
    Bookshelf.model.extend, rather than tacking them
    on myself.

--- models/user.js
+++ models/user.js
@@ -2,59 +2,55 @@
 
 var Bookshelf = require('bookshelf').conn,
     bcrypt = require('bcryptjs'),
-    _ = require('lodash')
+    _ = require('lodash'),
+    User
 
-var User = Bookshelf.Model.extend({
+User = Bookshelf.Model.extend({
     tableName: 'User',
-    hasTimestamps: ['createdAt', 'updatedAt'],
-    labels: function () {
-        return this.hasMany(Labels)
+    hasTimestamps: ['createdAt', 'updatedAt']
+}, {
+    createUser: function (values, next) {
+        bcrypt.hash(values.password, 10, function (err, hash) {
+            Bookshelf.knex('User').insert({
+                username: values.username,
+                email:    values.email,
+                password: hash
+            })
+            .exec(function (err, id) {
+                if (err) {
+                    if (err.hasOwnProperty('clientError')) {
+                        if (err.clientError.errno === 19) {
+                            return next(null, null, {message: 'Username already exists.'})
+                        }
+                    }
+                    // Raise unknown error
+                    return next(err, null, null)
+                }
+                else {
+                    new User({id: id[0]})
+                    .fetch()
+                    .then(function (user) {
+                        console.log(user)
+                        return next(null, user, null)
+                    })
+                }
+            })
+        })
+    },
+    findById: function (id, next) {
+        new this({id: id})
+            .fetch()
+            .then(function (user) {
+                next(null, user)
+            })
+    },
+    findByUsername: function (username, next) {
+        new this({username: username})
+            .fetch()
+            .then(function (user) {
+                next(null, user)
+            })
     }
 })
-
-User.createUser = function (values, next) {
-    bcrypt.hash(values.password, 10, function (err, hash) {
-        Bookshelf.knex('User').insert({
-            username: values.username,
-            email:    values.email,
-            password: hash
-        })
-        .exec(function (err, id) {
-            if (err) {
-                if (err.hasOwnProperty('clientError')) {
-                    if (err.clientError.errno === 19) {
-                        return next(null, null, {message: 'Username already exists.'})
-                    }
-                }
-                // Raise unknown error
-                return next(err, null, null)
-            }
-            else {
-                new User({id: id[0]})
-                .fetch()
-                .then(function (user) {
-                    console.log(user)
-                    return next(null, user, null)
-                })
-            }
-        })
-    })
-}
-
-User.findById = function (id, next) {
-    new this({id: id})
-        .fetch()
-        .then(function (user) {
-            next(null, user)
-        })
-}
-
-User.findByUsername = function (username, next) {
-    new this({username: username})
-        .fetch()
-        .then(function (user) {
-            next(null, user)
-        })
-}
 
 module.exports = Bookshelf.User = User
